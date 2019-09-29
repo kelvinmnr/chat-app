ba5d2478950cd4d26d72622d34c0944362721710
nenekelvin <karvinmitchar@gmail.com>
2019-09-29 08:01:07 +0300

    generated a new show and index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'chats/create'
+
+  get 'chats/new'
+
+  get 'chats/show'
+
+  get 'chats/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
